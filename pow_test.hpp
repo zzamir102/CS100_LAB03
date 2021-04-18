2098b4e33b078e8ac003abcb5b04ae289eb2c8b7
Zamir Noor <zamirnoor98@gmail.com>
2021-04-18 13:35:37 -0700

    Added more test cases to pow_test.hpp

--- pow_test.hpp
+++ pow_test.hpp
@@ -19,5 +19,39 @@ TEST(PowTest, PowEvaluateZero) {
 	EXPECT_EQ(test->evaluate(), 1.0);
 }
 
+TEST(PowTest, PowEvaluateNegative) {
+	Op* left = new Op(2.0);
+	Op* right = new Op(-2.0);
+	Pow* test = new Pow(left, right);
+	EXPECT_EQ(test->evaluate(), 0.25);
+}
+
+TEST(PowTest, PowStringifyNonZero) {
+	Op* left = new Op(3.0);
+	Op* right = new Op(6.0);
+	Pow* test = new Pow(left, right);
+	EXPECT_EQ(test->stringify(), "(3.000000 ** 6.000000)");
+}
+
+TEST(PowTest, PowStringifyMultChild) {
+	SevenEightMultMock* left = new SevenEightMultMock();
+	Op* leftPow = new Op(2.0);
+	Op* rightPow = new Op(3.0);
+	Pow* rightChild = new Pow(leftPow, rightPow);
+	Pow* test = new Pow(left, rightChild);
+	EXPECT_EQ(test->stringify(), "(56.000000 ** 8.000000)");
+}
+
+TEST(PowTest, PowEvaluateMultChild) {
+	SevenEightMultMock* left = new SevenEightMultMock();
+	Op* leftPow = new Op(2.0);
+	Op* rightPow = new Op(3.0);
+	Pow* rightChild = new Pow(leftPow, rightPow);
+	Pow* test = new Pow(left, rightChild);
+	EXPECT_EQ(test->evaluate(), 96717311574016);
+}
+
+
+
 
 #endif
