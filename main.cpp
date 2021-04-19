8b8c1dc9f59d225d9a97e6d333325e073f58fa10
Zamir Noor <zamirnoor98@gmail.com>
2021-04-18 18:21:51 -0700

    Final product - Updated CMakeLists.txt and main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 #include "base.hpp"
+#include "op.hpp"
+#include "sub.hpp"
+#include "mult.hpp"
+#include "pow.hpp"
+#include "rand.hpp"
+#include "div.hpp"
+#include "add.hpp"
+
+
 
 int main() {
     // This is a very basic main, and being able to correctly execute this main
